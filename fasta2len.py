b9578d3f601cda11126dd908c874915b2e5e7cf2
gaarangoa <gaarangoa@gmail.com>
2018-02-22 17:08:16 -0500

    fix

--- fasta2len.py
+++ fasta2len.py
@@ -7,5 +7,5 @@ sep = sys.argv[2] # separator to get only sequence id
 
 for record in SeqIO.parse(open(fi), "fasta"):
     id = record.id.split(sep)[0]
-    print(">"+id+"\t"+str(len(record.seq)))
+    print(id+"\t"+str(len(record.seq)))
 
