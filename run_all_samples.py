dd00add26ab8266f40f530563ea30928278a395f
gaarangoa <gaarangoa@gmail.com>
2018-03-01 11:43:32 -0500

    sample

--- run_all_samples.py
+++ run_all_samples.py
@@ -12,6 +12,8 @@ for sample,fastq1gz,fastq2gz in files:
     os.system('gunzip -c '+indir+"/"+fastq1gz+" > "+outdir+"/"+fastq1 )
     os.system('gunzip -c '+indir+"/"+fastq2gz+" > "+outdir+"/"+fastq2 )
 
-    os.system('chfilter remove --paired-1 '+outdir+'/'+fastq1+' --paired-2 '+outdir+'/'+fastq1+' --out-dir '+outdir )
+    os.system('chfilter remove --paired-1 '+outdir+'/'+fastq1+' --paired-2 '+outdir+'/'+fastq2+' --out-dir '+outdir )
 
-    os.system('randomfq.sh '+outdir+'/'+fastq1+'.no-chl.fastq '+fastq2+'.no-chl.fastq 12700000 0')
+    os.system('sh randomfq.sh '+outdir+'/'+fastq1+'.no-chl.fastq '+fastq2+'.no-chl.fastq 12700000 0')
+
+    os.system('rm '+outdir+"/"+fastq1+" "+outdir+"/"+fastq2+" bowtie*")
