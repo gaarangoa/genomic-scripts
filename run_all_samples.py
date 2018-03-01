c077708909c8a3dd164406884bd8e7662de68025
gaarangoa <gaarangoa@gmail.com>
2018-03-01 12:19:55 -0500

    ax

--- run_all_samples.py
+++ run_all_samples.py
@@ -8,12 +8,15 @@ indir = sys.argv[2]
 for sample,fastq1gz,fastq2gz in files:
     fastq1 = ".".join(fastq1gz.split('.')[:-1])
     fastq2 = ".".join(fastq2gz.split('.')[:-1])
-
+    print('uncompressing files')
     os.system('gunzip -c '+indir+"/"+fastq1gz+" > "+outdir+"/"+fastq1 )
     os.system('gunzip -c '+indir+"/"+fastq2gz+" > "+outdir+"/"+fastq2 )
-
+    print('running chfilter')
     os.system('chfilter remove --paired-1 '+outdir+'/'+fastq1+' --paired-2 '+outdir+'/'+fastq2+' --out-dir '+outdir )
-
-    os.system('sh randomfq.sh '+outdir+'/'+fastq1+'.no-chl.fastq '+fastq2+'.no-chl.fastq 12700000 0')
-
+    print('running randomfq')
+    os.system('sh randomfq.sh '+outdir+'/'+fastq1+'.no-chl.fastq '+outdir+'/'+fastq2+'.no-chl.fastq 12700000 0')
+    print('removing temp files')
     os.system('rm '+outdir+"/"+fastq1+" "+outdir+"/"+fastq2+' '+outdir+"/bowtie*")
+    print(':) done!')
+
+    
