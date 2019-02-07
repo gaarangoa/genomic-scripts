670004d90e9dfa0097819f3f5dc5d790f789fcf2
gaarangoa <gaarangoa@gmail.com>
2019-02-07 16:42:36 -0500

    added average

--- GeneTools/fasta2len.py
+++ GeneTools/fasta2len.py
@@ -20,10 +20,11 @@ def fasta2len(fasta_file, separator, label):
     for record in SeqIO.parse(open(fasta_file), "fasta"):
         id = record.id.split(separator)[label]
         try:
-            data[id]+=len(record.seq)
+            data[id]['len'] += len(record.seq)
+            data[id]['count'] += 1
         except Exception as e:
-            data[id] = len(record.seq)
+            data[id] = {"len": len(record.seq), "count": 1}
 
     for i in data:
-        print(i+'\t'+str(data[i]))
+        print(i+'\t'+str(data[i]['len']/data[i]['count']))
 
