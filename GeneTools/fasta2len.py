8c07cb8b802381ed185f60ff45cbda87f013eea6
gaarangoa <gaarangoa@gmail.com>
2019-02-07 16:39:47 -0500

    fix

--- GeneTools/fasta2len.py
+++ GeneTools/fasta2len.py
@@ -6,9 +6,9 @@ import click
 @click.command()
 @click.option('--fasta-file', required=True, help='fasta input file')
 @click.option('--separator', default="|", help='header separator [default: "|" ]')
-@click.option('--label-postition', default=0, help='label position [default: 0 ]')
+@click.option('--label', default=0, help='label position [default: 0 ]')
 
-def fasta2len(fasta_file, separator, label_position):
+def fasta2len(fasta_file, separator, label):
     '''
 
     Get the lengths of each sequence in a fasta file.
@@ -18,7 +18,7 @@ def fasta2len(fasta_file, separator, label_position):
     '''
     data = {}
     for record in SeqIO.parse(open(fasta_file), "fasta"):
-        id = record.id.split(separator)[label_position]
+        id = record.id.split(separator)[label]
         try:
             data[id]+=len(record.seq)
         except Exception as e:
